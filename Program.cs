7780a438d6d7c936c10a1972689f2cc6d9b76562
Matthew Jones <78270784+MattJones2000@users.noreply.github.com>
2025-09-24 19:34:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace PgnParser
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
+}
